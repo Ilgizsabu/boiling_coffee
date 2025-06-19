5c8535579c56727793be9d4adad145975f7f54e2
Saburo <barysaburov@gmail.com>
2025-06-19 21:45:23 +0300

    feat: js/css/html

--- src/index.js
+++ src/index.js
@@ -3,3 +3,13 @@ import './index.css';
 import 'mini.css/dist/mini-default.min.css';
 
 console.log('app started');
+
+function handleButtonColorPickerClick(event) {
+  event.currentTarget.style.backgroundColor = event.target.dataset.color;
+}
+
+const colorPicker = document.querySelector('.color-picker');
+const colorPickerNew = document.querySelector('.color-picker-new');
+
+colorPicker.addEventListener('click', handleButtonColorPickerClick);
+colorPickerNew.addEventListener('click', handleButtonColorPickerClick);
